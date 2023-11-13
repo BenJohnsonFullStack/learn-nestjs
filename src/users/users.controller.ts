dd6893a161a03b78653ba8d0a6cd9cc75dd8e880
Ben Johnson <monk.sound.design@gmail.com>
2023-11-12 20:02:05 -0500

    UsersService

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,9 +8,13 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users') // api route --->  /users
 export class UsersController {
+  // create instance of UsersService (note: this is a singleton in Nest and will pull in automatically if created elsewhere)
+  constructor(private readonly usersService: UsersService) {}
+
   /**
    * GET /users
    * GET /users/:id
@@ -20,35 +24,28 @@ export class UsersController {
    */
 
   @Get() // GET /users or /users?role=value
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [role];
+  getAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.usersService.getAll(role);
   }
 
   @Get(':id') // GET /users/:id
-  findById(@Param('id') id: string) {
-    return { id };
+  getById(@Param('id') id: string) {
+    // convert id paramter (a string) to a number
+    return this.usersService.getById(parseInt(id));
   }
 
   @Post() // POST /users
-  createUser(@Body() user: object) {
+  create(@Body() user: object) {
     return user;
   }
 
   @Patch(':id') // PATCH /users/:id
-  updateUser(@Param('id') id: string, @Body() updatedUser: object) {
-    return {
-      id,
-      message: `User Successfully Updated`,
-      data: { ...updatedUser },
-    };
+  update(@Param('id') id: string, @Body() updatedUser: object) {
+    return { id, ...updatedUser };
   }
 
   @Delete(':id') // DELETE /users/:id
-  deleteUser(@Param('id') id: string, @Body() deletedUser: object) {
-    return {
-      id,
-      message: `User Successfully Deleted`,
-      data: { ...deletedUser },
-    };
+  delete(@Param('id') id: string) {
+    return { id };
   }
 }
