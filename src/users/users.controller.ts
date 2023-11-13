43f0aed0060ceac4fc36aa992720433475e90198
Ben Johnson <monk.sound.design@gmail.com>
2023-11-12 19:10:28 -0500

    users CRUD endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // api route --->  /users
+export class UsersController {
+  /**
+   * GET /users
+   * GET /users/:id
+   * POST /users
+   * PATCH /users/:id
+   * DELETE /users/:id
+   */
+
+  @Get() // GET /users
+  findAll() {
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id
+  findById(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  createUser(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  updateUser(@Param('id') id: string, @Body() updatedUser: object) {
+    return {
+      id,
+      message: `User Successfully Updated`,
+      data: { ...updatedUser },
+    };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  deleteUser(@Param('id') id: string, @Body() deletedUser: object) {
+    return {
+      id,
+      message: `User Successfully Deleted`,
+      data: { ...deletedUser },
+    };
+  }
+}
