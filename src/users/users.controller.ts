5435a54b4c05dfa26664faaa3ccb4a9da4efa246
Ben Johnson <monk.sound.design@gmail.com>
2024-01-20 15:57:00 -0500

    users endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') //`${baseUrl}/users` (API endpoint)
+export class UsersController {
+  @Get() // GET /users
+  findAll() {
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() updatedUser: object) {
+    return { id, ...updatedUser };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  remove(@Param('id') id: string) {
+    return { id };
+  }
+}
