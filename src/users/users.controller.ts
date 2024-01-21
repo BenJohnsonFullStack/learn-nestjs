40fb80c1482037da4c74e3bb35154cf92c4025fe
Ben Johnson <monk.sound.design@gmail.com>
2024-01-21 12:40:42 -0500

    users controller using users service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,32 +8,50 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users') //`${baseUrl}/users` (API endpoint)
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get() // GET /users or /users?role=value
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+    return this.usersService.findAll(role);
   }
 
   @Get(':id') // GET /users/:id
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(parseInt(id));
   }
 
   @Post() // POST /users
-  create(@Body() user: object) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id') // PATCH /users/:id
-  update(@Param('id') id: string, @Body() updatedUser: object) {
-    return { id, ...updatedUser };
+  update(
+    @Param('id') id: string,
+    @Body()
+    updatedUser: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(parseInt(id), updatedUser);
   }
 
   @Delete(':id') // DELETE /users/:id
   remove(@Param('id') id: string) {
-    return { id };
+    return this.usersService.delete(parseInt(id));
   }
 }
